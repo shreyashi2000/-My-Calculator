eab79d73dbf622d45c872558c6614424e923bf60
shreyashi2000 <58186766+shreyashi2000@users.noreply.github.com>
2022-11-07 21:33:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons) .forEach((button)=>{
-    button.addEventListener('click' ,(e)=>{
-        if(e.target .innerHTML == '='){
-            string = eval(string);
-            document.querySelector('input').value = string;
-        }
-        else if (e.target.innerHTML == 'AC'){
-        string = ""
-        document.querySelector('input').value = string;
-        }
-        else{
-            console.log(e.target)
-            string = string + e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
-    })
-})
+let btn = document.querySelector('.btn');
+    btn.onclick = () => {
+    let txt = document.querySelector('.enter'
+    ).value;
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = 'en-US';
+    speech.text = txt;
+    speech.rate = 1;
+    speech.volume = 1;
+    speech.pitch = 1;
+
+     speechSynthesis.speak(speech);
+    }
